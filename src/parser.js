e401bd68927146211b7f665bfb120b95289851a1
Jannik Hollenbach <13718901+J12934@users.noreply.github.com>
2020-04-04 22:53:09 +0200

    Add finding category for deprecated tls versions

--- src/parser.js
+++ src/parser.js
@@ -1,29 +1,32 @@
 function parse(fileContent) {
     const serverScanResult = fileContent.server_scan_results[0];
+    
+    const partialFindings = [
+        generateInformationalServiceFinding(serverScanResult),
+        ...generateVulnarableTLSVersionFindings(serverScanResult)
+    ];
 
     const serverInfo = serverScanResult.server_info;
-
-    const { ip_address, hostname, port} = serverInfo.server_location
+    const { ip_address, hostname, port } = serverInfo.server_location;
     const location = `${hostname || ip_address}:${port}`;
-    return [
-        {
-            name: 'TLS Service',
-            description: '',
-            category: 'TLS Service Info',
-            severity: 'INFORMATIONAL',
+    
+    // Enhance partialFindings with common properties shared accross all sslyze findings
+    const findings = partialFindings.map(partialFinding => {
+        return {
             osi_layer: 'PRESENTATION',
-            hint: null,
             reference: null,
             location,
+            ...partialFinding,
             attributes: {
                 hostname,
                 ip_address,
                 port,
-                tls_versions: getAllSupportedTlsVersions(serverScanResult),
-                cipher_suites: getAllAcceptedCipherSuites(serverScanResult),
+                ...(partialFinding.attributes || {})
             },
-        },
-    ];
+        }
+    })
+
+    return findings;
 }
 
 module.exports.parse = parse;
@@ -33,13 +36,13 @@ function getTlsScanResultsAsArray(serverScanResult) {
     const commandResult = serverScanResult.scan_commands_results;
 
     return [
-        { name: "SSL 2.0", ...commandResult.ssl_2_0_cipher_suites},
-        { name: "SSL 3.0", ...commandResult.ssl_3_0_cipher_suites},
-        { name: "TLS 1.0", ...commandResult.tls_1_0_cipher_suites},
-        { name: "TLS 1.1", ...commandResult.tls_1_1_cipher_suites},
-        { name: "TLS 1.2", ...commandResult.tls_1_2_cipher_suites},
-        { name: "TLS 1.3", ...commandResult.tls_1_3_cipher_suites},
-    ]
+        { name: 'SSL 2.0', ...commandResult.ssl_2_0_cipher_suites },
+        { name: 'SSL 3.0', ...commandResult.ssl_3_0_cipher_suites },
+        { name: 'TLS 1.0', ...commandResult.tls_1_0_cipher_suites },
+        { name: 'TLS 1.1', ...commandResult.tls_1_1_cipher_suites },
+        { name: 'TLS 1.2', ...commandResult.tls_1_2_cipher_suites },
+        { name: 'TLS 1.3', ...commandResult.tls_1_3_cipher_suites },
+    ];
 }
 
 // Returns all supported cipher suites accoress all tls and ssl version as one big string array
@@ -49,14 +52,14 @@ function getAllAcceptedCipherSuites(serverScanResult) {
     // Use set to eliminate duplicates automatically
     const supportedVersions = new Set();
 
-    for(const tlsScanResult of tlsScanResults) {
+    for (const tlsScanResult of tlsScanResults) {
         for (const acceptedCipherSuit of tlsScanResult.accepted_cipher_suites) {
-            supportedVersions.add(acceptedCipherSuit.cipher_suite.openssl_name)
+            supportedVersions.add(acceptedCipherSuit.cipher_suite.openssl_name);
         }
     }
 
     // return set as a array
-    return [ ...supportedVersions.values() ];
+    return [...supportedVersions.values()];
 }
 
 // Returns all supported tls versions as a string array
@@ -65,12 +68,49 @@ function getAllSupportedTlsVersions(serverScanResult) {
 
     const supportedVersions = [];
 
-    for(const tlsScanResult of tlsScanResults) {
+    for (const tlsScanResult of tlsScanResults) {
         // Should have at least one accepted cipher suite to be considered "supported"
         if (tlsScanResult.accepted_cipher_suites.length > 0) {
-            supportedVersions.push(tlsScanResult.name)
+            supportedVersions.push(tlsScanResult.name);
         }
     }
 
     return supportedVersions;
+}
+
+function generateInformationalServiceFinding(serverScanResult) {
+    return {
+            name: 'TLS Service',
+            description: '',
+            category: 'TLS Service Info',
+            severity: 'INFORMATIONAL',
+            hint: null,
+            attributes: {
+                tls_versions: getAllSupportedTlsVersions(serverScanResult),
+                cipher_suites: getAllAcceptedCipherSuites(serverScanResult),
+            }
+        };
+}
+
+function generateVulnarableTLSVersionFindings(serverScanResult) {
+    const supportedTlsVersions = getAllSupportedTlsVersions(serverScanResult);
+
+    const VULNARABLE_VERSIONS = ['SSL 2.0', 'SSL 3.0', 'TLS 1.0'];
+
+    const findings = supportedTlsVersions
+        .filter(tlsVersion => VULNARABLE_VERSIONS.includes(tlsVersion))
+        .map(tlsVersion => {
+            return {
+                name: `TLS Version ${tlsVersion} is considered insecure`,
+                category: 'Outdated TLS Version',
+                description: 'The server uses outdated or unsecure tls versions.',
+                severity: 'MEDIUM',
+                hint: 'Upgrade to a higher tls version.',
+                attributes: {
+                    outdated_version: tlsVersion,
+                },
+            };
+        });
+
+    return findings;
 }
--- src/parser.test.js
+++ src/parser.test.js
@@ -69,4 +69,21 @@ test('parses result file for tls-v1-0.badssl.com:1010 correctly', async () => {
             cipher_suites: ["CAMELLIA256-SHA", "CAMELLIA128-SHA", "AES256-SHA", "AES128-SHA", "ECDHE-RSA-AES256-SHA", "ECDHE-RSA-AES128-SHA", "DHE-RSA-CAMELLIA256-SHA", "DHE-RSA-CAMELLIA128-SHA", "DHE-RSA-AES256-SHA", "DHE-RSA-AES128-SHA"],
         },
     });
+    
+    expect(findings).toContainEqual({
+        name: 'TLS Version TLS 1.0 is considered insecure',
+        category: 'Outdated TLS Version',
+        description: 'The server uses outdated or unsecure tls versions.',
+        severity: 'MEDIUM',
+        hint: 'Upgrade to a higher tls version.',
+        osi_layer: 'PRESENTATION',
+        reference: null,
+        location: 'tls-v1-0.badssl.com:1010',
+        attributes: {
+            hostname: 'tls-v1-0.badssl.com',
+            ip_address: '104.154.89.105',
+            port: 1010,
+            outdated_version: 'TLS 1.0'
+        },
+    });
 });
